4582945df83c6a33b0a96e6ba6658e7686c6c5fa
Steven Nathaniel <steven.nathaniel@ui.ac.id>
2023-05-31 17:43:21 +0700

    first commit

--- app/src/main/java/com/stevennt/movemate/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/stevennt/movemate/ui/detail/DetailActivity.kt
@@ -1,12 +1,42 @@
-package com.stevennt.movemate.ui.auth.detail
+package com.stevennt.movemate.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.stevennt.movemate.R
+import com.stevennt.movemate.data.model.Workouts
+import com.stevennt.movemate.databinding.ActivityDetailBinding
+import com.stevennt.movemate.databinding.ActivityMainBinding
 
+@Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        // Hide the status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.backDetail.setOnClickListener{
+            onBackPressed()
+        }
+
+        val dataWorkouts = intent.getParcelableExtra<Workouts>("key_workouts")!!
+
+        binding.tvDetail.text = dataWorkouts.name
+        binding.ivDetail.setImageResource(dataWorkouts.icon)
+        binding.tvInstruction.text = dataWorkouts.instruction
+        dataWorkouts.focusArea?.let { binding.ivMuscleDetail.setImageResource(it) }
+
     }
 }
--- app/src/main/java/com/stevennt/movemate/ui/home/HomeActivity.kt
+++ app/src/main/java/com/stevennt/movemate/ui/home/HomeActivity.kt
@@ -1,12 +1,61 @@
 package com.stevennt.movemate.ui.home
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.stevennt.movemate.R
+import com.stevennt.movemate.data.model.Workouts
+import com.stevennt.movemate.databinding.ActivityDetailBinding
+import com.stevennt.movemate.databinding.ActivityHomeBinding
 
+@Suppress("DEPRECATION")
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var rvWorkout: RecyclerView
+    private val list = ArrayList<Workouts>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        rvWorkout = binding.rvListWorkout
+        list.addAll(getListWorkout())
+        showRecyclerList()
+    }
+
+    @SuppressLint("Recycle")
+    private fun getListWorkout(): ArrayList<Workouts> {
+        val dataName = resources.getStringArray(R.array.workout_name_detail)
+        val dataIcon = resources.obtainTypedArray(R.array.workout_icon)
+        val dataInstruction = resources.getStringArray(R.array.workout_instruction)
+        val dataFocusArea = resources.obtainTypedArray(R.array.workout_focus_area)
+        val listWorkout = ArrayList<Workouts>()
+        for (i in dataName.indices) {
+            val workout = Workouts(dataName[i], dataIcon.getResourceId(i, -1), dataInstruction[i], dataFocusArea.getResourceId(i, -1))
+            listWorkout.add(workout)
+        }
+        return listWorkout
+    }
+
+    private fun showRecyclerList() {
+        rvWorkout.layoutManager = LinearLayoutManager(this)
+        val listWorkoutAdapter = ListWorkoutAdapter(list)
+        rvWorkout.adapter = listWorkoutAdapter
+        rvWorkout.setHasFixedSize(true)
     }
 }
